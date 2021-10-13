65b6e9fcbd76eb8654b2bb3868f16ae8f43c5bc0
Allen Sanders <asanders@gamuttechnologysvcs.com>
2021-10-13 12:49:53 -0400

    Updates

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
-const port = 3000;
+const port = 5001;
 app.use(cors());
 app.get('/random', (req, res) => res.json({
     number:Math.floor(Math.random()*Math.floor(100))
